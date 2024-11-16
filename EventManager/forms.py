ba7971d63bf98ec851926959f1dc430eca92d7fd
Jason Thomas <jayt1012520@yahoo.com>
2024-11-16 12:41:13 -0600

    update

--- EventManager/forms.py
+++ EventManager/forms.py
@@ -3,19 +3,25 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import EventComment, Venue
 from .models import Event
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
 
 class RegisterForm(UserCreationForm):
     email = forms.EmailField()
 
     class Meta:
         model = User
-        fields = ["username","first_name", "last_name", "email", "password1", "password2"]
+        fields = ["username", "first_name", "last_name", "email", "password1", "password2"]
+
 
 class EventCommentForm(forms.ModelForm):
     class Meta:
         model = EventComment
         fields = ['text']
         widgets = {
+<<<<<<< Updated upstream
             'text': forms.Textarea(attrs={'rows': 4, 'placeholder': 'Enter your comment...'}),
         }
 
@@ -23,8 +29,26 @@ class VenueDetailsForm(forms.ModelForm):
     class Meta:
         model = Venue
         fields = ['name', 'image', 'description', 'address_line_1', 'address_line_2', 'city', 'state', 'country', 'zipcode']
+=======
+            'text': forms.Textarea(attrs={'rows': 4, 'placeholder': 'Enter your comment...'})
+
+        }
+
+
+class VenueDetailsForm(forms.ModelForm):
+    class Meta:
+        model = Venue
+        fields = ['name', 'image', 'description', 'address_line_1', 'address_line_2', 'city', 'state', 'country',
+                  'zipcode']
+
+>>>>>>> Stashed changes
 
 class EventForm(forms.ModelForm):
     class Meta:
         model = Event
+<<<<<<< Updated upstream
         fields = ['name', 'description', 'date']
+=======
+
+        fields = ['name', 'description', 'date']
+>>>>>>> Stashed changes
