edd92249959d690c702aee6d1ce25f1e22959f85
acg-UNO <aconcepcion@unomaha.edu>
2024-11-16 21:00:03 +0100

    organizational updates

--- EventManager/urls.py
+++ EventManager/urls.py
@@ -2,25 +2,26 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Index and Event URLs
     path('', views.index, name='index'),
-
     path('events/', views.events, name='events'),
-    path('events/<id>/', views.eventdetails, name='eventdetails'),
-    path('events/register/<id>/', views.eventregister, name='eventregister'),
-    path('events/unregister/<id>/', views.eventunregister, name='eventunregister'),
+    path('events/add/', views.add_event, name='add_event'),
+    path('events/<int:id>/', views.eventdetails, name='eventdetails'),
+    path('events/<int:id>/edit/', views.edit_event, name='edit_event'),
+    path('events/register/<int:id>/', views.eventregister, name='eventregister'),
+    path('events/unregister/<int:id>/', views.eventunregister, name='eventunregister'),
 
+    # Venue URLs
     path('venues/', views.venues, name='venues'),
-    path('venues/<id>/', views.venuedetails, name='venuedetails'),
+    path('venues/<int:id>/', views.venuedetails, name='venuedetails'),
+    path('venues/add/', views.addvenue, name='addvenue'),
+    path('venues/<int:id>/edit/', views.editvenuedetails, name='editvenue'),
+    path('venues/<int:id>/delete/', views.deletevenue, name='deletevenue'),
 
+    # Account and Registration
     path('register/', views.register, name='register'),
     path('account/', views.account, name='account'),
 
-    path('venue/add', views.addvenue, name='addvenue'),
-    path('venues/edit/<id>/', views.editvenuedetails, name='editvenue'),
-    path('venues/delete/<id>/', views.deletevenue, name='deletevenue'),
+    # Comments
     path('delete_comment/<int:comment_id>/', views.delete_comment, name='delete_comment'),
-    path('events/', views.events, name='events'),
-    path('events/add/', views.add_event, name='add_event'),
-    path('events/<int:event_id>/', views.event_detail, name='event_detail'),
-    path('events/<int:event_id>/edit/', views.edit_event, name='edit_event'),
-    ]
+]
